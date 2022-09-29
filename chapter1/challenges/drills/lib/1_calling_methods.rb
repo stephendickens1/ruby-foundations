ddf0743a0efe296a293f2dfef8f7bd3e6f8f363c
Stephen Dickens <s.dickens87@gmail.com>
2022-09-29 22:51:17 +0100

    2_challenge_complete

--- chapter1/challenges/drills/lib/1_calling_methods.rb
+++ chapter1/challenges/drills/lib/1_calling_methods.rb
@@ -12,49 +12,61 @@
 # when the string is 'hello world'
 # the method should return 'HELLO WORLD'
 def block_caps_a_string(string)
-  # your code goes here
+  return string.upcase
 end
+
+puts block_caps_a_string("hello world")
 
 # TASK: define a method that returns a string that is the same as a given string, but converted to lower-case characters.
 # EXAMPLE INPUT/OUTPUT:
 # when the string is 'HELLO WORLD'
 # the method should return 'hello world'
 def lower_case_a_string(string)
-  # your code goes here
+  return string.downcase
 end
+
+puts lower_case_a_string("HELLO WORLD")
 
 # TASK: define a method that returns a number which is the count of all characters in a given string.
 # EXAMPLE INPUT/OUTPUT:
 # when the string is 'hello'
 # the method should return 5
 def length_of_a_string(string)
-  # your code goes here
+  return string.length
 end
+
+puts length_of_a_string("hello")
 
 # TASK: define a method that returns a string that reverses the order of the characters in a given string.
 # EXAMPLE INPUT/OUTPUT:
 # when the string is 'hello'
 # the method should return 'olleh'
 def reverse_a_string(string)
-  # your code goes here
+  return string.reverse
 end
+
+puts reverse_a_string("hello")
+
 
 # TASK: define a method that returns a string that inverts the case of each character in a given string.
 # EXAMPLE INPUT/OUTPUT:
 # when the string is 'Hello World'
 # the method should return 'hELLO wORLD'
 def swap_the_case_of_a_string(string)
-  # your code goes here
+  return string.swapcase
 end
+
+puts  swap_the_case_of_a_string("Hello World")
 
 # TASK: define a method that returns a symbol that has the same characters (single words for now).
 # EXAMPLE INPUT/OUTPUT:
 # when the string is 'hello'
 # the method should return :hello
 def string_to_symbol(string)
-  # your code goes here
+  return string.to_sym
 end
 
+puts  string_to_symbol("hello")
 # Integer methods
 
 # TASK: define a method that returns a boolean evaluates if a given integer is odd.
@@ -64,9 +76,10 @@ end
 # when the integer is 2
 # the method should return false
 def is_integer_odd?(integer)
-  # your code goes here
+  return integer.odd?
 end
 
+puts  is_integer_odd?(1)
 # TASK: define a method that returns a boolean evaluates if a given integer is even.
 # EXAMPLE INPUT/OUTPUT:
 # when the integer is 1
@@ -74,40 +87,51 @@ end
 # when the integer is 2
 # the method should return true
 def is_integer_even?(integer)
-  # your code goes here
+  return integer.even?
 end
+
+puts is_integer_even?(1)
 
 # TASK: define a method that returns a float converted from a given integer.
 # EXAMPLE INPUT/OUTPUT:
 # when the integer is 1
 # the method should return 1.0
 def integer_to_float(integer)
-  # your code goes here
+  return integer.to_f
 end
+
+puts integer_to_float(10)
 
 # TASK: define a method that returns a string equivalent of a given integer.
 # EXAMPLE INPUT/OUTPUT:
 # when the integer is 1
 # the method should return '1'
 def integer_to_string(integer)
-  # your code goes here
+  return integer.to_s
 end
+
+puts integer_to_float(10)
 
 # TASK: define a method that returns an integer that is one less than a given integer (don't worry about negative numbers for now).
 # EXAMPLE INPUT/OUTPUT:
 # when the integer is 2
 # the method should return 1
 def return_previous_integer(integer)
-  # your code goes here
+  return integer.pred
 end
+
+puts return_previous_integer(2)
 
 # TASK: define a method that returns an integer that is one more than a given integer (don't worry about negative numbers for now).
 # EXAMPLE INPUT/OUTPUT:
 # when the integer is 1
 # the method should return 2
 def return_next_integer(integer)
-  # your code goes here
+  return integer.next
+
 end
+puts return_next_integer(1)
+
 
 # Float methods
 
@@ -116,32 +140,42 @@ end
 # when the float is 1.1
 # the method should return 2.0
 def round_up(float)
-  # your code goes here
+  return float.ceil.to_f
 end
+
+puts round_up(1.1)
 
 # TASK: define a method that returns a float that is rounded down from a given float.
 # EXAMPLE INPUT/OUTPUT:
 # when the float is 1.1
 # the method should return 1.0
 def round_down(float)
-  # your code goes here
+  return float.floor.to_f
+ 
 end
+
+puts round_down(1.1)
 
 # TASK: define a method that returns a string that is equivalent to a given float.
 # EXAMPLE INPUT/OUTPUT:
 # when the float is 1.1
 # the method should return '1.1'
 def float_to_string(float)
-  # your code goes here
+  return float.to_s
 end
+
+puts float_to_string(1.1)
+
 
 # TASK: define a method that returns a integer that is converted from a given float.
 # EXAMPLE INPUT/OUTPUT:
 # when the float is 1.1
 # the method should return 1
 def float_to_integer(float)
-  # your code goes here
+  return float.to_int
 end
+
+puts float_to_integer(1.1)
 
 # TASK: define a method that returns a boolean that evaluates whether or not a given float is positive.
 # EXAMPLE INPUT/OUTPUT:
@@ -150,8 +184,10 @@ end
 # when the float is -1.1
 # the method should return false
 def float_is_positive?(float)
-  # your code goes here
+ return float.positive?
 end
+
+puts float_is_positive?(0.0)
 
 # TASK: define a method that returns a boolean that evaluates whether or not a given float is negative.
 # EXAMPLE INPUT/OUTPUT:
@@ -160,9 +196,9 @@ end
 # when the float is -1.1
 # the method should return true
 def float_is_negative?(float)
-  # your code goes here
+  return float.negative?
 end
-
+puts float_is_negative?(0.0)
 # Symbol methods
 
 # TASK: define a method that returns a symbol that inverts the case of each character for a given symbol.
@@ -170,33 +206,44 @@ end
 # when the symbol is :Hello
 # the method should return :hELLO
 def swap_the_case_of_a_symbol(symbol)
-  # your code goes here
+  return symbol.swapcase
+ 
 end
+
+puts swap_the_case_of_a_symbol(:Hello)
 
 # TASK: define a method that returns a string that is the equivalent of a given symbol.
 # EXAMPLE INPUT/OUTPUT:
 # when the symbol is :hello
 # the method should return 'hello'
 def symbol_to_string(symbol)
-  # your code goes here
+  return symbol.to_s
+
 end
+
+puts symbol_to_string(:hello)
 
 # TASK: define a method that returns a symbol that converts every character of a given symbol to upper case.
 # EXAMPLE INPUT/OUTPUT:
 # when the symbol is :hello
 # the method should return :HELLO
 def block_caps_a_symbol(symbol)
+  return symbol.upcase
   # your code goes here
 end
+
+puts block_caps_a_symbol(:hello)
 
 # TASK: define a method that returns a symbol that converts every character of a given symbol to lower case.
 # EXAMPLE INPUT/OUTPUT:
 # when the symbol is :HELLO
 # the method should return :hello
 def lower_case_a_symbol(symbol)
-  # your code goes here
+  return symbol.downcase
+  
 end
 
+puts lower_case_a_symbol(:HELLO)
 # Boolean methods
 
 # TASK: define a method that returns a string which is equivalent to a given boolean.
@@ -206,6 +253,7 @@ end
 # when the boolean is false
 # the method should return false
 def boolean_to_string(boolean)
-  # your code goes here
+ return boolean.to_s
 end
 
+puts boolean_to_string(true)
--- chapter1/challenges/drills/lib/2_calling_methods_with_args.rb
+++ chapter1/challenges/drills/lib/2_calling_methods_with_args.rb
@@ -7,8 +7,10 @@
 # when the string is Apple
 # it should return false
 def starts_with_the_letter_a?(string)
-  # your code goes here
+  return string.start_with?("a")
 end
+
+puts starts_with_the_letter_a?("Apple")
 
 # TASK: define a method that returns a boolean depending on whether or not a given string ends in a lowercase 'a'.
 # EXAMPLE INPUT/OUTPUT:
@@ -17,8 +19,10 @@ end
 # when the string is JAVA
 # the method should return false
 def ends_with_the_letter_a?(string)
-  # your code goes here
+  return string.end_with?("a")
 end
+
+puts ends_with_the_letter_a?("Java")
 
 # TASK: define a method that returns a boolean depending on whether or not a given string contains the substring 'hello'.
 # EXAMPLE INPUT/OUTPUT:
@@ -27,45 +31,66 @@ end
 # when the string is 'world'
 # the method should return false
 def contains_hello?(string)
-  # your code goes here
+
+return string.include?("hello")
+  
 end
+
+puts contains_hello?("hello world")
 
 # TASK: define a method that returns a string that replaces the substring 'hello' with the substring 'goodbye'.
 # EXAMPLE INPUT/OUTPUT:
 # when the string is 'hello folks'
 # the method should return 'goodbye folks'
 def substitute_hello_with_goodbye(string)
-  # your code goes here
+  return string.gsub("hello", "goodbye")
 end
+
+puts substitute_hello_with_goodbye("hello folks")
+
 
 # TASK: define a method that returns a string that has removed all vowels from a given string.
 # EXAMPLE INPUT/OUTPUT:
 # when the string is 'hullabaloo'
 # the method should return 'hllbl'
 def remove_all_vowels(string)
-  # your code goes here
+  return string.tr('aeiou', '')
 end
+
+puts remove_all_vowels("hullabaloo")
 
 # TASK: define a method that returns a string that has removed all consonants from a given string.
 # EXAMPLE INPUT/OUTPUT:
 # when the string is 'hullabaloo'
 # the method should return 'uaaoo'
 def remove_all_consonants(string)
-  # your code goes here
+return string.tr('^aeiou', '') 
 end
+
+puts remove_all_consonants("hullabaloo")
+# Very useful find on stack overflow. "If the first argument of the tr method of String starts with ^, then it denotes all characters except those listed.""
 
 # TASK: define a method that returns a string that has removed the last half of characters from a given string.
 # EXAMPLE INPUT/OUTPUT:
 # when the string is 'coding'
 # the method should return 'cod'
 def first_half(string)
-  # your code goes here
+  return string[0..-string.length/2-1]
 end
+
+puts first_half("coding")
+#This was very, very difficult for me. I knew there was a way to get there but how? I really struggled here. Was this something that has been covered that I should have easily understood or was this expected to be a slog? Not a criticism, just mildly concerned for my own ability here / if I missed something!
+
+
 
 # TASK: define a method that returns a string that has removed the first half of characters from a given string.
 # EXAMPLE INPUT/OUTPUT:
 # when the string is 'coding'
 # the method should return 'ing'
 def second_half(string)
-  # your code goes here
+  return string.reverse[0..-string.length/2-1].reverse
 end
+
+puts second_half("coding")
+
+# this is so unbelievably scuffed. I accept this. But it DOES work. I have no idea how I'd refactor this and I'm sure it's really, really inefficient. I really feel like I've missed something very obvious here, so any help would be very much appreciated
