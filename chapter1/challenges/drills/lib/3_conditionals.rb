5f7c97836428c84625adad6e32f8761c6d03fe1a
Stephen Dickens <s.dickens87@gmail.com>
2022-10-02 19:08:58 +0100

    completed drills

--- chapter1/challenges/drills/lib/3_conditionals.rb
+++ chapter1/challenges/drills/lib/3_conditionals.rb
@@ -5,9 +5,22 @@
 # return '1' if the number is not divisible by 3, 5 or 15
 # clue - you can check divisibility using modulo (%)
 def fizz_buzz(number)
-  # your code goes here
-end
+  if (number%15 == 0)
+    return "fizzbuzz"
+  elsif (number%3 == 0)
+    return "fizz"
+  elsif (number%5 == 0)
+    return "buzz"
+  else
+    return "1"
+  end
+end 
+  
+  puts fizz_buzz(30)
 
+
+
+  
 # if the greeting is 'good morning'
 # return 'good morning to you too'
 # if the greeting is 'hello'
@@ -15,21 +28,43 @@ end
 # if the greeting is anything else
 # return the greeting that was received
 def reply_to(greeting)
-  # your code goes here
-end
+  if greeting == "good morning"
+    return "good morning to you too"
+  elsif greeting == "hello"
+    return "hi"
+  else 
+    return greeting
+  end 
+end 
+
+puts reply_to("bonjour")
+ 
+
 
 # when the number is greater than or equal to 10
 # deduct 10 and return the result
 # when the number is below 10
 # return the number
 def deduct_10_if_possible(number)
-  # your code goes here
+  if (number >= 10)
+    return (number-10)
+  else
+    return number
+  end
 end
+puts deduct_10_if_possible(8)
+
 
 # if the number is below 100
 # return 100
 # if the number is above 100
 # return the number
 def top_up_to_100(number)
-  # your code goes here
+  if number < 100
+    return 100
+  else number > 100
+    return number
+  end 
 end
+
+puts top_up_to_100(99)
--- chapter1/challenges/drills/lib/4_defining_methods.rb
+++ chapter1/challenges/drills/lib/4_defining_methods.rb
@@ -24,45 +24,103 @@
 # say_hello
 # takes no args (arguments)
 # returns 'hello'
+def say_hello
+    return "hello"
+end
+
+puts say_hello
 
 # say_goodbye
 # takes no args
 # returns 'goodbye'
+
+def say_goodbye
+    return "goodbye"
+end
+
+puts say_goodbye
 
 # say_hello_to
 # takes one string as an arg
 # example: the arg is `Sam`
 # returns `Hello, Sam`
 
+def say_hello_to(name)
+    return "Hello, #{name}"
+end
+
+puts say_hello_to("Sam")
+
 # say_goodbye_to
 # takes one string as an arg
 # example: the arg is `Sam`
 # returns `Goodbye, Sam`
+def say_goodbye_to(name)
+    return "Goodbye, #{name}"
+end
+
+puts say_goodbye_to("Sam")
 
 # square
 # takes one number as an arg
 # multiplies that number by itself
 # returns the new value
 
+def square(number)
+    return (number*number)
+end
+
+puts square(3)
+
+
+
 # divisible_by_three?
 # takes one number as an arg
 # returns true if the number is divisible by three
 # returns false if the number is not divisible by three
+
+def divisible_by_three?(number)
+   if number%3 == 0
+    return true
+   else
+    return false
+   end
+ end
+
+ puts divisible_by_three?(3)
 
 # add
 # takes two numbers as args
 # adds them together
 # returns the total
 
+def add(number1, number2)
+    return number1 + number2
+end 
+
+puts add(1, 2)
+
 # multiply
 # takes two numbers as args
 # multiplies one by the other
 # returns the result
 
+def multiply(number1, number2)
+    return number1 * number2
+end 
+
+puts multiply(1, 2)
+
 # add_number_strings
 # takes two numbers as strings
 # converts them both to numbers
 # returns the total
+
+def add_number_strings(number1, number2)
+    return  number1.to_i + number2.to_i
+end 
+
+puts add_number_strings("20", "35")
 
 # multiply_number_strings
 # takes two numbers as strings for args
@@ -70,25 +128,55 @@
 # multiplies one by the other
 # returns the result
 
+def multiply_number_strings(number1, number2)
+    return  number1.to_i * number2.to_i
+end 
+
+puts multiply_number_strings("20", "35")
+
 # both_odd?
 # takes two numbers as args
 # returns true if both are odd
 # otherwise, returns false
+
+def both_odd?(number1, number2)
+    return number1.odd? && number2.odd?
+end
+
+puts both_odd?(3, 5)
 
 # both_even?
 # takes two numbers as args
 # returns true if both are even
 # otherwise, returns false
 
+def both_even?(number1, number2)
+    return number1.even? && number2.even?
+end
+
+puts both_even?(3, 5)
+
 # one_odd?
 # takes two numbers as args
 # returns true if at least one of them is odd
 # otherwise, returns false
 
+def one_odd?(number1, number2)
+    return number1.odd? || number2.odd?
+end
+
+puts one_odd?(3, 5)
+
 # one_even?
 # takes two numbers as args
 # returns true if at least one of them is even
 # otherwise returns false
+def one_even?(number1, number2)
+    return number1.even? || number2.even?
+end
+
+puts one_even?(3, 5)
+
 
 # rev_sym_caps
 # takes one string as an arg
@@ -96,9 +184,25 @@
 # converts it to a symbol
 # returns the symbol in block caps
 
+def rev_sym_caps(string)
+    return string.reverse.to_sym.upcase
+end
+
+puts rev_sym_caps("triceratops")
+
 # truncate
 # takes one string as an arg
 # if the string is longer than 10 characters
 # returns the first 10 chars followed by '...'
 # if the string is 10 chars or less
 # returns the whole string
+
+def truncate(string)
+    if string.length > 10
+        return string[0...10]+"..."
+    else 
+        return string
+    end 
+end 
+
+puts truncate("Hi there friendos!")
